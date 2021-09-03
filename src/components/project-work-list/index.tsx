58ebd0a3fd5e7a6cf840cba4326b004338d7838d
Maciek Baron <thebezet@gmail.com>
2021-09-03 10:42:08 +0100

    Fix work copy

--- src/components/project-work-list/index.tsx
+++ src/components/project-work-list/index.tsx
@@ -12,6 +12,7 @@ interface ProjectWorkListProps {
             mainImage: any;
             slug: string;
             title: string;
+            description: string;
           }
         }
       }[]
@@ -24,13 +25,19 @@ const ProjectWorkListPure = (
 ) => (
   <ul className="project-work-list">
     { edges
-      .map(({ node: { frontmatter: { slug, title, mainImage } } }) => (
+      .map(({
+        node: {
+          frontmatter: {
+            slug, title, mainImage, description,
+          },
+        },
+      }) => (
         <li key={slug}>
           <ProjectItem
             title={title}
             image={mainImage}
             slug={slug}
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+            description={description}
           />
         </li>
       )) }
@@ -62,6 +69,7 @@ const ProjectWorkList = () => (
             }
             slug
             title
+            description
           }
         }
       }
