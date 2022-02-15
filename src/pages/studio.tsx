986e0fb3c8af7d605f547e0b724b1b1d5f164c75
Igor Strapko <igor@goodpraxis.coop>
2022-02-15 15:21:22 +0000

    Add more copy for the Studio page

--- src/pages/studio.tsx
+++ src/pages/studio.tsx
@@ -54,6 +54,70 @@ const IndexPage = () => {
         </div>
       </section>
       <section className="grid">
+        <div className="main-description">
+          <h2 className="heading">Our services</h2>
+          <p>
+            We offer a range of digital services, taking advantage of the
+            latest frameworks and technologies. We advocate open-source
+            solutions wherever possible – our core expertise lies in Django,
+            React, Wordpress and Gatsby.js. However we always carefully choose
+            the right technology for each project, whether it’s a website,
+            application, game, Twitch bot, experimental interactive experience
+            or anything else our clients require.
+          </p>
+          <p>
+            The projects we work on range from small microsites to large web
+            apps. We are highly experienced at building bespoke applications
+            to suit large or complex requirements. These projects often require
+            us to integrate or build different APIs and services, deliver
+            custom content management solutions and meet all compliance and
+            accessibility requirements.
+          </p>
+          <p>
+            We work with experienced designers who can not only take care of
+            the design but also branding aspects of projects.
+          </p>
+          <p>
+            As an
+            {' '}
+            <a href="https://www.gather.town/partner/good-praxis">
+              official Gather Town partner
+            </a>
+            , we have experience in designing and building
+            {' '}
+            <a href="https://goodpraxis.coop/studio/">
+              custom Gather maps
+            </a>
+            {' '}
+            for offices, events and campaigns. By using the Gather API, we can
+            create bespoke elements, automated events and services, and run
+            games and activities.
+          </p>
+          <h2 className="heading">What we offer:</h2>
+          <ul>
+            <li>Website design and development</li>
+            <li>Web app development, including mobile</li>
+            <li>Content Management Systems (CMS)</li>
+            <li>Databases and API infrastructure</li>
+            <li>DevOps - CI/CD - Hosting - Support</li>
+            <li>Data Integration and Connection</li>
+            <li>Office tasks automatisation</li>
+            <li>Social Media Bots</li>
+            <li>Gather Town maps design and development</li>
+          </ul>
+          <h2 className="heading">What we use:</h2>
+          <ul>
+            <li>HTML / CSS</li>
+            <li>JavaScript &amp; TypeScript</li>
+            <li>React, Gatsby.js</li>
+            <li>Python, Flask, Django</li>
+            <li>SQL, MySQL, PostgreSQL, MongoDB</li>
+            <li>Adobe Suite</li>
+            <li>Gather Town</li>
+            <li>WordPress</li>
+            <li>REST API, GraphQL</li>
+          </ul>
+        </div>
         <div id="contact-us" className="contact-us">
           <h2 className="heading">Get in touch</h2>
           <p className="main-description">
